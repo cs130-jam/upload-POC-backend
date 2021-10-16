3cadf30f5b771a11f07c7311bbfca97332d22138
Andrew Cummings <acummings@egnyte.com>
2021-10-16 02:31:51 -0700

    proof of concept for using filesystem + nginx for file upload

---
+++ src/test/java/com/example/uploadpocbackend/UploadPocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uploadpocbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadPocBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
